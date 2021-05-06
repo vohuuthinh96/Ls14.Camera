05c21bfdc7badac39bec7d5df502af16cc1af51d
thinhvh <thinhvohuu@vccorp.vn>
2021-05-06 15:25:33 +0700

    thinhvh

--- app/src/main/java/com/atoproduction/ls14camera/MainActivity.java
+++ app/src/main/java/com/atoproduction/ls14camera/MainActivity.java
@@ -1,36 +1,75 @@
 package com.atoproduction.ls14camera;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private String currentPhotoPath;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_PICK_IMAGE = 2;
+    private ImageView mImageView;
+    private Button btnGetPhoto;
+    private Button btnTakePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        dispatchTakePictureIntent();
-        dispatchTakePictureIntent();
+        mImageView = findViewById(R.id.image);
+        btnGetPhoto = findViewById(R.id.btn_get_photo);
+        btnTakePhoto = findViewById(R.id.btn_take_photo);
+
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        btnGetPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickImage();
+            }
+        });
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_PICK_IMAGE);
+    }
 
-//    private void dispatchTakePictureIntent() {
+//    private void dispatchTakePictureIntentNoSave() {
 //        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 //        try {
 //            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -39,9 +78,7 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
-
     private File createImageFile() {
-        // Create an image file name
         try {
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             String imageFileName = "JPEG_" + timeStamp + "_";
@@ -52,27 +89,23 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             return null;
         }
-
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (Exception ex) {
-                // Error occurred while creating the File
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                currentPhotoPath = photoFile.getAbsolutePath();
-                Uri photoURI = FileProvider.getUriForFile(this, "com.atoproduction.ls14camera.fileprovider", photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (Exception ex) {
+            // Error occurred while creating the File
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            currentPhotoPath = photoFile.getAbsolutePath();
+            Uri photoURI = FileProvider.getUriForFile(this, "com.atoproduction.ls14camera.fileprovider", photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
@@ -82,8 +115,22 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 //            Bundle extras = data.getExtras();
 //            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            imageView.setImageBitmap(imageBitmap);
+//            ( (ImageView)findViewById(R.id.image)).setImageBitmap(imageBitmap);
             Log.d("thinhvh", "onActivityResult: " + currentPhotoPath);
+            Glide.with(MainActivity.this).load(new File(currentPhotoPath)).into(mImageView);
+        }
+
+        if (requestCode == REQUEST_PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    final Uri imageUri = data.getData();
+                    final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    mImageView.setImageBitmap(selectedImage);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
